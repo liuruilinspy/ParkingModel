03f0d97b10cf1b2fd712d91115f3840ccccab3fa
yangyu <yangyu9415@gmail.com>
2016-10-12 05:14:54 -0400

    update search7

--- simulation/search7.py
+++ simulation/search7.py
@@ -383,7 +383,7 @@ if __name__ == "__main__":
     # row, column, parking_row, parking_column
     row, column, parking_row, parking_column = 2, 2, 1, 1
 
-    print("--- generate graph ---", datetime.datetime.now())
+    print("-- generate graph --", datetime.datetime.now())
     nodeset, d_size, p_size, n_size = generate_graph(row, column, parking_row, parking_column)
     total_spots = d_size + p_size + n_size
 
@@ -409,47 +409,30 @@ if __name__ == "__main__":
 
     # all the paths between all node pairs
     drive_nodeset = [(key, val) for key, val in nodeset.items() if val.type == "D" or val.type == "C"]
-    print("--- search all path in graph ---", datetime.datetime.now())
+    print("-- search all path in graph --", datetime.datetime.now())
     all_pair = all_path(drive_nodeset)
 
     # used as threshold when entering the parking lot
     default_best_cost = 5 * (len(shortest_path(all_pair, enter_node, exit_node)) - 1) * w_cost
 
-    # simple test
-
-    # # occupancy density
-    # density = 50
-    # # stop forward search threshold
-    # saving_threshold = 1
-    # # visited node
-    # knowledge = [-1] * 291
-    # # used as threshold when entering the parking lot
-    # default_best_cost = 250
-    #
-    # sigma = 1
-    #
-    # spot_map, spot_pdf = generate_gaussian_map(nodeset, all_pair, exit_node, sigma, density / 100, total_spots)
-    #
-    # for x in ["G", 0.1, 0.2, 0.3, 0.4, 0.5]:
-    #     best_node, path, back_steps = execute(spot_map, nodeset, all_pair, knowledge, enter_node, exit_node,
-    #                                             x, d_cost, w_cost, u_cost, default_best_cost, saving_threshold)
-    #     print("Final:", path[-1], "ParkOption:", path[-1].empty_parking_spot(spot_map), "Length:", len(path),
-    #           "BackSteps:", back_steps, "Cost:", total_cost(path, all_pair, exit_node, d_cost, w_cost, u_cost))
-    #
-
-    fo = open("2_2.txt", "w")
+    fo = open("2_2_1.txt", "w")
     fo.write(
-        "map \t density \t saving_threshold \t x_value \t cost \t back_steps \t final_position \t final_parking_options \t path_length \t path \t map \n")
+        "map \t density \t saving_threshold \t x_value \t cost \t cost-truth \t back_steps \t final_position \t final_parking_options \t path_length \t path \t map \n")
+    for saving_threshold in [4, 8, 12, 16]:
+        f_cost = open("2_2_cost_" + str(saving_threshold), "w")
+        f_cost.write("density \t ground_truth \t historical \t 10% \t 20% \t 30% \t 40% \t 50% \n")
+        print("-- Threshold", saving_threshold)
+        for density in range(10, 100, 10):
+            print("  -- Density", density, datetime.datetime.now())
+            print("    -- Progress [", end="", flush=True)
 
-    for density in range(10, 100, 10):
-        print("--- Density", density, datetime.datetime.now())
-        print("     -- Progress [", end="", flush=True)
-        for i in range(100):
-            if i % 10 == 0:
-                print("=", end="", flush=True)
-            spot_map, spot_pdf = generate_gaussian_map(nodeset, all_pair, exit_node, sigma, density / 100, total_spots)
-            xs = ["G", spot_pdf, 0.1, 0.2, 0.3, 0.4, 0.5]
-            for saving_threshold in [2, 5, 10, 15, 20]:
+            for i in range(10):
+                if i % 10 == 0:
+                    print("=", end="", flush=True)
+                spot_map, spot_pdf = generate_gaussian_map(nodeset, all_pair, exit_node, sigma, density / 100, total_spots)
+                xs = ["G", spot_pdf, 0.1, 0.2, 0.3, 0.4, 0.5]
+                truth = 0
+                print_result = []
                 for j in range(len(xs)):
                     key = str(j) + str(saving_threshold)
                     knowledge = [-1] * total_spots
@@ -457,19 +440,31 @@ if __name__ == "__main__":
                                                                exit_node, xs[j], d_cost, w_cost, u_cost,
                                                                default_best_cost, saving_threshold)
                     cost = total_cost(prev_path, all_pair, exit_node, d_cost, w_cost, u_cost)
+                    if j == 0:
+                        truth = cost
+                        print_result.append(truth)
+                    else:
+                        print_result.append(cost - truth)
                     # print("Final:", prev_path[-1], "ParkOption:", prev_path[-1].empty_parking_spot(spot_map), "Length:",
                     #       len(prev_path), "BackSteps:", back_steps, "Cost: ", cost)
                     fo.write(str([row, column, parking_row, parking_column])
-                                + "\t" + str(density)
-                                + "\t" + str(saving_threshold)
-                                + "\t" + str(j)
-                                + "\t" + str(cost)
-                                + "\t" + str(back_steps)
-                                + "\t" + str(prev_path[-1])
-                                + "\t" + str(prev_path[-1].empty_parking_spot(spot_map))
-                                + "\t" + str(len(prev_path))
-                                + "\t" + str(list(map(lambda n: n.id, prev_path)))
-                                + "\t" + str(spot_map) + "\n")
-        print("]")
+                             + "\t" + str(density)
+                             + "\t" + str(saving_threshold)
+                             + "\t" + str(j)
+                             + "\t" + str(cost)
+                             + "\t" + str(cost - truth)
+                             + "\t" + str(back_steps)
+                             + "\t" + str(prev_path[-1])
+                             + "\t" + str(prev_path[-1].empty_parking_spot(spot_map))
+                             + "\t" + str(len(prev_path))
+                             + "\t" + str(list(map(lambda n: n.id, prev_path)))
+                             + "\t" + str(spot_map) + "\n")
+
+                f_cost.write(str(density)
+                             + "\t"
+                             + "\t".join(map(str, print_result))
+                             + "\n")
+            print("]")
+        f_cost.close()
     fo.close()
 
